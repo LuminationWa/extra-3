36ffa5d76c1cb4927ad5df6f81412f5650c27032
Edel <edelmedina322@gmail.com>
2023-06-07 18:34:56 -0300

    Add new icons / Update styles

--- front-end/src/Components/Gachapon.js
+++ front-end/src/Components/Gachapon.js
@@ -1,25 +1,26 @@
 import plusIcon from "../Assets/icons/plus.svg";
 import closeIcon from "../Assets/icons/x.svg";
+import gemIcon from "../Assets/icons/gems.png";
 import heroImg from "../Assets/images/mick-haupt-KtTF68ZjBak-unsplash.jpg";
+import logo from "../Assets/icons/logo.png";
 const Gachapon = () => {
   return (
     <div className="gachapon">
       <div className="gacha-top">
         <div className="gacha-top-decoration">
-          <img alt="logo"></img>
-          <h1>SUPER EPIC GACHA</h1>
+          <img alt="logo" src={logo}></img>
         </div>
         <div className="gacha-top-right">
           <div className="gacha-top-user-info">
             <div className="user-currency">
-              <img alt="currency"></img>
-              <button className="btn-no-style">User currency</button>
+              <img src ={gemIcon} alt="currency" className="icon"></img>
+              <button className="btn-no-style">(3490)</button>
             </div>
-            <button className="btn-no-style">
+            <button className="btn-no-style btn-icon">
               <img src={plusIcon} alt="plus"></img>
             </button>
           </div>
-          <button className="btn-no-style">
+          <button className="btn-no-style btn-icon">
             <img src={closeIcon} alt="close"></img>
           </button>
         </div>
