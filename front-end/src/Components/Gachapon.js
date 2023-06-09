9ec45457e28a6824e4dc79cc9a067e2f37d1f894
Edel <edelmedina322@gmail.com>
2023-06-09 13:19:16 -0300

    Update styles

--- front-end/src/Components/Gachapon.js
+++ front-end/src/Components/Gachapon.js
@@ -10,26 +10,28 @@ const Gachapon = () => {
         <div className="gacha-top-decoration">
           <img alt="logo" src={logo}></img>
         </div>
-        <div className="gacha-top-right">
-          <div className="gacha-top-user-info">
-            <div className="user-currency">
-              <img src ={gemIcon} alt="currency" className="icon"></img>
-              <button className="btn-no-style">(3490)</button>
-            </div>
-            <button className="btn-no-style btn-icon">
-              <img src={plusIcon} alt="plus"></img>
-            </button>
-          </div>
-          <button className="btn-no-style btn-icon">
-            <img src={closeIcon} alt="close"></img>
-          </button>
-        </div>
+        <button className="btn-no-style btn-icon close-icon">
+          <img src={closeIcon} alt="close"></img>
+        </button>
       </header>
       <div className="gacha-main">
         <img className="banner-img" src={heroImg} alt="Banner"></img>{" "}
-        <div className="gacha-buttons">
-          <button className="primary-btn">Single</button>
-          <button className="primary-btn">Tenfold</button>
+        <div className="main-right">
+          <div className="gacha-buttons">
+            <div className="gacha-currency">
+              <div className="gacha-currency-info">
+                <div className="user-currency">
+                  <img src={gemIcon} alt="currency" className="icon"></img>
+                  <button className="btn-no-style">(3490)</button>
+                </div>
+                <button className="btn-no-style btn-icon">
+                  <img src={plusIcon} alt="plus"></img>
+                </button>
+              </div>
+            </div>
+            <button className="primary-btn">Single</button>
+            <button className="primary-btn">Tenfold</button>
+          </div>
         </div>
       </div>
     </div>
